2aa7d242522899498e82f88b09ca37c78f649add
akash.ju.msc@gmail.com <akash.ju.msc@gmail.com>
2023-10-10 09:18:34 +0600

    worked multiple app

--- frontend/vue-project-one/vite.config.js
+++ frontend/vue-project-one/vite.config.js
@@ -2,9 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        feature: resolve(__dirname/feature, 'feature/index.html'),
+      }
+    }
+  },
   plugins: [
     vue(),
   ],
