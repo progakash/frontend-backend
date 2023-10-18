88ad52ddf26881266e7f70f2d0624bd0956ead55
akash.ju.msc@gmail.com <akash.ju.msc@gmail.com>
2023-10-18 09:18:02 +0600

    workign template

--- frontend/vue-project-two/src/main.js
+++ frontend/vue-project-two/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import router from './routes'
 import store from './stores'
-import './style.css'
 import App from './App.vue'
 
 createApp(App)
--- frontend/vue-project-two/src/pages/FeatureOne.vue
+++ frontend/vue-project-two/src/pages/FeatureOne.vue
@@ -1,5 +1,38 @@
 <template>
-    <h1>Feature One</h1>
+   <header>
+       <div class="container">
+            <div class="header-top">
+                <h1>CompanyName</h1>
+                <a href="#">Sign In</a>
+            </div>
+            <nav>
+                <a href="#">Sign In</a>
+                <a href="#">Sign In</a>
+                <a href="#">Sign In</a>
+            </nav>
+       </div>
+   </header>
+   <div class="container">
+    <div class="left">
+        <a href="#">Feature one</a>
+    </div>
+   </div>
 </template>
 
-<script></script>
+<script>
+</script>
+<style>
+header {
+    background-color: brown;
+}
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+}
+body {
+  margin: 0;
+}
+h1 {
+    margin: 0;
+}
+</style>
